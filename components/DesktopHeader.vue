b256f03b3befd3dada9e80a45fc67a8f200604f9
mikamidies <mikamidies04@gmail.com>
2024-04-30 00:19:48 +0500

    Firts

--- components/DesktopHeader.vue
+++ components/DesktopHeader.vue
@@ -7,22 +7,26 @@
             <NuxtLink to="/">Главная</NuxtLink>
           </li>
           <li>
-            <NuxtLink to="/">О нас</NuxtLink>
+            <NuxtLink to="/about">О нас</NuxtLink>
           </li>
           <li>
-            <NuxtLink to="/">Вузы и специальности</NuxtLink>
+            <NuxtLink to="/universities">Вузы и специальности</NuxtLink>
           </li>
           <li>
-            <NuxtLink to="/">Услуги</NuxtLink>
+            <NuxtLink to="/services">Услуги</NuxtLink>
           </li>
           <li>
-            <NuxtLink to="/">Контакты</NuxtLink>
+            <NuxtLink to="/contacts">Контакты</NuxtLink>
           </li>
           <li>
             <a-dropdown>
               <a-menu slot="overlay">
-                <a-menu-item key="1"> 1st menu item </a-menu-item>
-                <a-menu-item key="2"> 2nd menu item </a-menu-item>
+                <a-menu-item key="1">
+                  <NuxtLink to="/">Base 1</NuxtLink>
+                </a-menu-item>
+                <a-menu-item key="2">
+                  <NuxtLink to="/">Base 2</NuxtLink>
+                </a-menu-item>
               </a-menu>
               <a-button style="margin-left: 8px">
                 База знаний <a-icon type="down" />
@@ -75,21 +79,25 @@ export default {
   z-index: 999;
   transition: 0.3s;
 }
+
 .scroll,
 .white {
   background: white;
   border-bottom: 1px solid #ebebeb;
 }
+
 .scroll .links a,
 .scroll .num,
 .white .links a,
 .white .num {
   color: var(--black);
 }
+
 .scroll .container,
 .white .container {
   padding: 16px 0;
 }
+
 .container {
   display: flex;
   align-items: center;
@@ -98,26 +106,31 @@ export default {
   padding: 24px 0;
   transition: 0.3s;
 }
+
 .links,
 .right {
   display: flex;
   align-items: center;
   gap: 40px;
 }
+
 .stick {
   width: 1px;
   height: 24px;
   background: rgba(255, 255, 255, 0.4);
 }
+
 .links a,
 .num {
   color: var(--White, #fff);
   font-size: 16px;
   font-style: normal;
   font-weight: 400;
-  line-height: 140%; /* 22.4px */
+  line-height: 140%;
+  /* 22.4px */
   transition: 0.3s;
 }
+
 .scroll .stick,
 .white .stick {
   background: var(--black);
---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="master">
+
+    </div>
+</template>
+
+<script>
+export default {
+    layout: "inner",
+}
+</script>
+
+<style scoped></style>
