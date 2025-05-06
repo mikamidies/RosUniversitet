b78b6cb63940b8769617905890c12bd5aa32c4da
Davronali <davronali84@gmail.com>
2025-05-06 18:56:57 +0500

    replain id changed

--- plugins/chat.js
+++ plugins/chat.js
@@ -1,4 +1,4 @@
-window.replainSettings = { id: '1c33d61d-dddb-4473-8826-093676580b18' };
+window.replainSettings = { id: '7adf9185-7965-40ce-9862-15ea5482a0bb' };
 (function(u){var s=document.createElement('script');s.async=true;s.src=u;
 var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
 })('https://widget.replain.cc/dist/client.js');
